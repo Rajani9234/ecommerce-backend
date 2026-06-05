f3bd282b367fd4fc357b07b9fc5885cd5a823fb2
Rajani Kumari <your_email@example.com>
2026-06-05 15:29:46 +0530

    updated

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+
+const app = express();
+
+dotenv.config();
+
+connectDB(); // <- ye line MUST hai
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
+
+
+
